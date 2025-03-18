41335a8b0aff0fa547094106cdb028abf4725b60
Gui Omena <gui.omenas@gmail.com>
2025-03-17 22:17:17 -0300

    Seller entity class / Classe seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -8,6 +11,10 @@ public class Program {
 
 	Department obj = new Department(1, "books");
 	System.out.println(obj);
+	
+	Seller seller = new Seller(12, "omena", "Omena@@@", new Date(), 2000.0, obj);
+	
+	System.out.println(seller);
 	}
 	
 }
